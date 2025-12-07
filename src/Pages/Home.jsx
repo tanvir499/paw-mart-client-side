ec20639f34266bbd52f4190472aac7bea05be349
Tanvir <126787226+tanvir499@users.noreply.github.com>
2025-12-07 19:37:03 +0600

    update home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from '../Component/Slider';
+import PopularSection from '../Component/PopularSection';
+
+
+const Home = () => {
+    return (
+        <div>
+           <Slider></Slider>
+           <PopularSection></PopularSection>
+        </div>
+    );
+};
+
+export default Home;
